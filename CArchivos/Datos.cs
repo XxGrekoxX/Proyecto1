8cef393e7a07f4e5add75f41fe9e1ce069a05b29
Caasi855G <119400077@cuc.cr>
2024-10-28 20:57:37 -0600

    CArchivos datos y xml

--- CArchivos/Datos.cs
+++ CArchivos/Datos.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using System.Xml.Linq;
 
 
@@ -11,11 +12,11 @@ namespace CArchivos
 {
     public class Datos
     {
-        private string xmlFilePath = "Proyecto1\\CArchivos\\Usuarios.xmll";
-        public Boolean ValidarLogin(string Usern, string Password) 
+        string xmlFilePath = Path.Combine("CArchivos", "Usuarios.xml");
+        public Boolean ValidarLogin(string Usern, string Password)
         {
             XDocument ValDoc = XDocument.Load(xmlFilePath);
-
+            
             var user = ValDoc.Descendants("User ").FirstOrDefault(u=>u.Element("Username").Value == Usern && u.Element("Password").Value == Password);
             return user != null;
         }
